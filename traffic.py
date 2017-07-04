dba46dd14141ec51d8c42799a9988c54676d279c
MartiniMoe <martini.moe@cheex.de>
2017-07-04 23:56:57 +0200

    make snmp stuf object oriented

--- traffic.py
+++ traffic.py
@@ -4,7 +4,8 @@ import socket
 import sys
 import gc
 
-def main():
+class Traffic:
+
     agent_ip = "10.23.42.254"
     agent_port = 161
     agent_community = "public"
@@ -16,54 +17,66 @@ def main():
     oid_uptime = "1.3.6.1.2.1.1.3.0"
     oid_speed = "1.3.6.1.2.1.2.2.1.5.9"
 
-    gc.collect()
+    s = None
+    greq = None
+    gresp = None
+    last_ut = None
+    last_in8 = None
+    speed = None
 
-    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    s.settimeout(1)
-
-    greq = usnmp.SnmpPacket(type=usnmp.SNMP_GETREQUEST, community=agent_community, id=time.ticks_us())
-    for i in (oid_speed, oid_uptime, oid_if_inoct):
-        greq.varbinds[i] = None
-
-    s.sendto(greq.tobytes(), (agent_ip, agent_port))
-    d = s.recvfrom(1024)
-    gresp = usnmp.SnmpPacket(d[0])
-
-    last_ut = gresp.varbinds[oid_uptime][1]
-    last_in8 = gresp.varbinds[oid_if_inoct][1]
-
-    while True:
+    def __init__(self):
         gc.collect()
 
-        speed = gresp.varbinds[oid_speed][1]
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.s.settimeout(1)
 
-        time.sleep(delay)
+        self.greq = usnmp.SnmpPacket(type=usnmp.SNMP_GETREQUEST, community=self.agent_community, id=time.ticks_us())
+        for i in (self.oid_speed, self.oid_uptime, self.oid_if_inoct):
+            self.greq.varbinds[i] = None
 
-        greq.id=time.ticks_us()
-        s.sendto(greq.tobytes(), (agent_ip, agent_port))
+        self.s.sendto(self.greq.tobytes(), (self.agent_ip, self.agent_port))
+        d = self.s.recvfrom(1024)
+        self.gresp = usnmp.SnmpPacket(d[0])
+
+        self.last_ut = self.gresp.varbinds[self.oid_uptime][1]
+        self.last_in8 = self.gresp.varbinds[self.oid_if_inoct][1]
+
+    def get_traffic(self):
+        gc.collect()
+
+        self.speed = self.gresp.varbinds[self.oid_speed][1]
+
+        time.sleep(self.delay)
+
+        self.greq.id = time.ticks_us()
+        self.s.sendto(self.greq.tobytes(), (self.agent_ip, self.agent_port))
+
         try:
-            d = s.recvfrom(1024)
+            d = self.s.recvfrom(1024)
 
-            gresp = usnmp.SnmpPacket(d[0])
+            self.gresp = usnmp.SnmpPacket(d[0])
 
-            if greq.id == gresp.id:
-                ut = gresp.varbinds[oid_uptime][1]
-                in8 = gresp.varbinds[oid_if_inoct][1]
+            if self.greq.id == self.gresp.id:
+                ut = self.gresp.varbinds[self.oid_uptime][1]
+                in8 = self.gresp.varbinds[self.oid_if_inoct][1]
 
-                if in8 != last_in8:
-                    print("new traffic value.")
+                if in8 != self.last_in8:
+                    #print("new traffic value.")
 
-                    timediff = ut - last_ut
-                    traffic = in8 - last_in8
+                    timediff = ut - self.last_ut
+                    traffic = in8 - self.last_in8
 
-                    mbps = (traffic * 8 * 100) / (timediff/100 * speed)
+                    mbps = (traffic * 8 * 100) / (timediff/100 * self.speed)
 
-                    print("  -->  " + str(timediff/100) + " seconds")
-                    print("  -->  " + str(mbps) + " Mb/s")
+                    #print("  -->  " + str(timediff/100) + " seconds")
+                    #print("  -->  " + str(mbps) + " Mb/s")
 
-                    last_in8 = in8
-                    last_ut = ut
+                    self.last_in8 = in8
+                    self.last_ut = ut
+
+                    return mbps
 
         except Exception as e:
             print("error")
             print(e.args[0])
+            return 0
