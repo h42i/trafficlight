b17dd5abb0016b63d3480e0e22c9c7c769a9aec8
smeik <smeik@hasi.it>
2017-07-05 19:58:51 +0200

    comments on lights.py for help()

--- lights.py
+++ lights.py
@@ -11,24 +11,28 @@ class Lights:
         self.apa = APA102(clock, data, 56)
 
     def set_low_load(self):
+        '''define color for green-light'''
         self.set_bottom_color((255, 255, 255, 31))
         self.set_middle_color((0, 0, 0, 0))
         self.set_top_color((0, 0, 0, 0))
         self.write_pixels()
 
     def set_middle_load(self):
+        '''define color for mid-red-light'''
         self.set_bottom_color((0, 0, 0, 0))
         self.set_middle_color((255, 255, 255, 31))
         self.set_top_color((0, 0, 0, 0))
         self.write_pixels()
 
     def set_high_load(self):
+        '''define color for top-red-light'''
         self.set_bottom_color((0, 0, 0, 0))
         self.set_middle_color((0, 0, 0, 0))
         self.set_top_color((255, 255, 255, 31))
         self.write_pixels()
 
     def write_pixels(self):
+        '''write pixels^^'''
         self.apa.write()
 
     def set_all_color(self, color):
@@ -38,13 +42,16 @@ class Lights:
         self.write_pixels()
 
     def set_bottom_color(self, color):
+        '''sets the green-light'''
         for i in range(0, 19):
             self.apa[i] = color
 
     def set_middle_color(self, color):
+        '''sets the mid-red-light'''
         for i in range(19, 37):
             self.apa[i] = color
 
     def set_top_color(self, color):
+        '''sets the top-red-light'''
         for i in range(37, 56):
             self.apa[i] = color
