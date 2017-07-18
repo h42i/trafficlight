d2fe201f093ead7297948a14ebd8f8af1cf806e1
MartiniMoe <martini.moe@cheex.de>
2017-07-18 21:45:17 +0200

    cleanup main.py | add gitignore | :poop:

--- main.py
+++ main.py
@@ -1,78 +1,117 @@
 import network
-from traffic import *
 import time
-from lights import *
 from umqtt.simple import MQTTClient
+from lights import *
+from traffic import *
 
-# Setup Network
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-if not wlan.isconnected():
-    print('connecting to network...')
-    wlan.connect('HaSi-Kein-Internet-Legacy', 'bugsbunny')
-    while not wlan.isconnected():
-        pass
-print('network config:', wlan.ifconfig())
+# Config
+wifi_ssid = "HaSi-Kein-Internet-Legacy"
+wifi_psk = "bugsbunny"
+mqtt_server = "atlas.hasi"
+mqtt_client_name = "traffic_light"
+mqtt_topic = "hasi/lights/traffic_light"
 
-mqtt_message = None
-# Triggered by c.check_msg()
-def sub_cb(topic, msg):
-    global mqtt_message
-    mqtt_message = str(msg, "utf-8")
+# State
+lights_on = True
+mqtt_client = None
+snmp_traffic = None
+light = None
 
+# Set everything up
+def setup():
+    global wifi_ssid
+    global wifi_psk
 
-server = "atlas.hasi"
-c = MQTTClient("traffic_light", server)
-c.set_callback(sub_cb)
-c.connect()
-c.subscribe(b"hasi/lights/traffic_light")
+    global mqtt_topic
+    global mqtt_server
+    global mqtt_client
+    global mqtt_client_name
 
-tr = Traffic()
-light = Lights()
+    global snmp_traffic
+    global light
+
+    # Setup Network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(wifi_ssid, wifi_psk)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+    # Setup MQTT
+    mqtt_client = MQTTClient(mqtt_client_name, mqtt_server)
+    mqtt_client.set_callback(mqtt_callback)
+    mqtt_client.connect()
+    mqtt_client.subscribe(bytes(mqtt_topic, "utf-8"))
+
+    # Setup remaining stuff
+    snmp_traffic = Traffic()
+    light = Lights()
+
+# MQTT-Callback; Triggered by c.check_msg()
+def mqtt_callback(topic, msg):
+    global lights_on
+
+    message = str(msg, "utf-8")
+
+    if message == "on":
+        lights_on = True
+    elif message == "off":
+        lights_on = False
 
 # Test routine
-print('testing...')
-light.set_all_color((0, 0, 0, 0))
-time.sleep(0.5)
-light.set_low_load()
-time.sleep(0.2)
-light.set_middle_load()
-time.sleep(0.2)
-light.set_high_load()
-time.sleep(0.2)
-light.set_all_color((255, 255, 255, 31))
-time.sleep(0.2)
-light.set_all_color((0, 0, 0, 0))
-time.sleep(0.2)
-light.set_all_color((255, 255, 255, 31))
-time.sleep(0.2)
-light.set_all_color((0, 0, 0, 0))
-light.set_low_load()
-print('testing complete')
+def test_lights():
+    global light
 
-while True:
-    #check for mqtt-messages
-    c.check_msg()
-    if mqtt_message == "off":
-        light.set_all_color((0, 0, 0, 0))
-        while True:
-            time.sleep(1)
-            c.check_msg()
-            if mqtt_message == "on":
-                break
-    
-    # Light to the traffic
-    time.sleep(1)
+    print('testing...')
+    light.set_all_color((0, 0, 0, 0))
+    time.sleep(0.5)
+    light.set_low_load()
+    time.sleep(0.2)
+    light.set_middle_load()
+    time.sleep(0.2)
+    light.set_high_load()
+    time.sleep(0.2)
+    light.set_all_color((255, 255, 255, 31))
+    time.sleep(0.2)
+    light.set_all_color((0, 0, 0, 0))
+    time.sleep(0.2)
+    light.set_all_color((255, 255, 255, 31))
+    time.sleep(0.2)
+    light.set_all_color((0, 0, 0, 0))
+    light.set_low_load()
+    print('testing complete')
 
-    traffic = tr.get_traffic()
+# Work work work...
+def loop():
+    global mqtt_client
+    global lights_on
+    global light
+    global snmp_traffic
 
-    if traffic != 0 and traffic != None:
-        if traffic < 5:
-            light.set_low_load()
-        elif traffic < 10:
-            light.set_middle_load()
-        elif traffic > 14:
-            light.set_chaos_load()
+    while True:
+        #check for mqtt-messages
+        time.sleep(1)
+        mqtt_client.check_msg()
+
+        if not lights_on:
+            light.set_all_color((0, 0, 0, 0))
         else:
-            light.set_high_load()
-c.disconnect()
+            traffic = snmp_traffic.get_traffic()
+
+            if traffic != 0 and traffic != None:
+                if traffic < 5:
+                    light.set_low_load()
+                elif traffic < 10:
+                    light.set_middle_load()
+                elif traffic > 14:
+                    light.set_chaos_load()
+                else:
+                    light.set_high_load()
+
+setup()
+test_lights()
+loop()
+mqtt_client.disconnect()
